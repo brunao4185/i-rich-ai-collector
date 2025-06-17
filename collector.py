e0b9e8516381183a8b12e656e01dd388329d6f85
brunao4185 <brunoartioli@yahoo.com>
2025-06-17 00:24:16 -0300

    collector.py

--- collector.py
+++ collector.py
@@ -43,5 +43,13 @@ def last_entries():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+@app.route('/history', methods=['GET'])
+def full_history():
+    try:
+        df = pd.read_csv(CSV_FILE)
+        return df.to_json(orient='records')
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
